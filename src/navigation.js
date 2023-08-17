8a663d7e1dc08cc944ffac504f933bf9a4e694b7
jesus <jesus.ramon2192@gmail.com>
2023-08-16 18:14:13 -0600

    Navegacion

---
+++ src/navigation.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', navigator, false);
+window.addEventListener('hashchange', navigator, false);
+
+function navigator() {
+    console.log({location});
+
+    location.hash.startsWith('#perfil')
+    ? perfilPage()  :
+    location.hash.startsWith('#blog')
+    ? blogPage()    :
+    location.hash.startsWith('#blogs')
+    ? blogsPage()   :
+    homePage();
+};
+
+const homePage = () => {
+    console.log('Home');
+};
+
+const perfilPage = () => {
+    console.log('PerfilPage');
+};
+
+const blogPage = () => {
+    console.log('BlogPage');
+};
+
+const blogsPage = () => {
+    console.log('BlogsPage');
+};
